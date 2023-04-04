80e5bff36b86b604c2658d6edb770d9fd7da6ef7
Mike Heavers <mheavers@gmail.com>
2023-04-04 10:15:52 -0700

    add navigation between sections - need to process openAI requests at each stage

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,8 +4,68 @@ const init = () => {
   //fetchOpenAI();
 };
 
+const gotoSection = (sectionID) => {
+  const currentButton = document.querySelector(`.form-group-button.active`);
+  const currentID = parseInt(currentButton.dataset.id);
+  const currentSection = document.querySelector(
+    `.form-group[data-id="${currentID}"]`
+  );
+
+  const nextSection = document.querySelector(
+    `.form-group[data-id="${sectionID}"]`
+  );
+  const nextButton = document.querySelector(
+    `.form-group-button[data-id="${sectionID}"]`
+  );
+  nextButton.classList.toggle("active", true);
+  currentButton.classList.toggle("active", false);
+  currentSection.classList.toggle("hidden", true);
+  nextSection.classList.toggle("hidden", false);
+};
+
+const gotoNextSection = (currentID) => {
+  const currentSection = document.querySelector(
+    `.form-group[data-id="${currentID}"]`
+  );
+
+  const curNavButton = document.querySelector(
+    `.form-group-button[data-id="${currentID}"]`
+  );
+  const nextSection = document.querySelector(
+    `.form-group[data-id="${currentID + 1}"]`
+  );
+  const nextNavButton = document.querySelector(
+    `.form-group-button[data-id="${currentID + 1}"]`
+  );
+  if (nextSection) {
+    currentSection.classList.toggle("hidden", true);
+    nextSection.classList.toggle("hidden", false);
+    curNavButton.classList.toggle("active", false);
+    if (nextNavButton) {
+      nextNavButton.classList.toggle("active", true);
+    } else {
+      console.log("end");
+    }
+  }
+};
+
 const addEventListeners = () => {
   document.getElementById("submit").addEventListener("click", handleFormSubmit);
+  const nextButtons = document.querySelectorAll(".next");
+  nextButtons.forEach((nextButton) => {
+    nextButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      gotoNextSection(parseInt(e.target.closest(".form-group").dataset.id));
+    });
+  });
+  const navButtons = document.querySelectorAll(".form-group-button");
+  navButtons.forEach((navButton) => {
+    navButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      const sectionNumber = parseInt(e.target.dataset.id);
+      gotoSection(sectionNumber);
+    });
+  });
 };
 
 const createImageGroups = (imageGroups) => {
